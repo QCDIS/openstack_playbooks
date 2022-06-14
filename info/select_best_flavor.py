fa1a952a0588ee8c6c8dcbd82b7e8511c10303f0
skoulouzis <koulouzis@gmail.com>
2022-06-14 13:42:56 +0200

    moved playbooks from github.com/QCDIS/playbooks.git

---
+++ info/select_best_flavor.py
@@ -0,0 +1,48 @@
+import json
+import os
+import sys
+import tempfile
+
+import numpy as np
+from numpy.linalg import norm
+
+
+def get_shorter_dist(available_instances, requested_instances):
+    flavors = available_instances['openstack_flavors']
+    selected_flavors = {}
+    for requested_instance_name in requested_instances:
+        min_dist = sys.maxsize
+        requested_instance = requested_instances[requested_instance_name]
+        requested_vector = np.array([int(requested_instance['mem_size'].split(' ')[0]),
+                                     int(requested_instance['num_cores']),
+                                     int(requested_instance['disk_size'].split(' ')[0])])
+        for flavor in flavors:
+            available_vector = np.array([flavor['ram'], flavor['vcpus'], flavor['disk']])
+            dist = norm(requested_vector - available_vector)
+            if dist < min_dist:
+                min_dist = dist
+                selected_flavor = {'flavor_id': flavor['id']}
+                selected_flavors[requested_instance_name] = selected_flavor
+
+    return selected_flavors
+
+
+if __name__ == "__main__":
+    available_instances_file_path = sys.argv[1]
+    requested_instances_file_path = sys.argv[2]
+
+    f = open(available_instances_file_path, )
+    available_instances = json.load(f)
+
+    f = open(requested_instances_file_path, )
+    requested_instances = json.load(f)
+
+    selected_flavors = get_shorter_dist(available_instances, requested_instances)
+    instances = {'selected_flavors':selected_flavors}
+
+    fd, filename = tempfile.mkstemp()
+
+    with os.fdopen(fd, 'w') as tmp:
+        # do stuff with temp file
+        json.dump(instances, tmp)
+    print(filename)
